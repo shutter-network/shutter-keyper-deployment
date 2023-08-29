cc2eb36b34fcc5de1184c2fb5db3d31b91a42ae3
Konrad Feldmeier <konrad@brainbot.com>
2023-08-29 14:02:16 +0200

    Add port for external SM address

--- scripts/shuttermint_config.sh
+++ scripts/shuttermint_config.sh
@@ -5,6 +5,6 @@ CFG=/data/chain/config/config.toml
 sed -i "/^bootstrap-peers =/c\bootstrap-peers = \"${SEED_NODES}\"" $CFG
 sed -i "/^moniker =/c\moniker = \"${SHUTTERMINT_MONIKER}\"" $CFG
 sed -i "/^genesis-file =/c\genesis-file = \"/info/genesis.json\"" $CFG
-sed -i "/^external-address =/c\external-address = \"${PUBLIC_IP}\"" $CFG
+sed -i "/^external-address =/c\external-address = \"${PUBLIC_IP}:26656\"" $CFG
 sed -i "/^addr-book-strict =/c\addr-book-strict = true" $CFG
 sed -i "/^pex =/c\pex = true" $CFG
