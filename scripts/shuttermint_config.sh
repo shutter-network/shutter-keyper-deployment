7a8af316df6014c3e2c13c0caaf4cbf1946a19ce
Ulrich Petri <ulo@ulo.pe>
2023-09-18 18:37:48 +0200

    Make metrics configurable and add tendermint metrics

--- scripts/shuttermint_config.sh
+++ scripts/shuttermint_config.sh
@@ -8,3 +8,7 @@ sed -i "/^genesis-file =/c\genesis-file = \"/info/genesis.json\"" $CFG
 sed -i "/^external-address =/c\external-address = \"${PUBLIC_IP}:26656\"" $CFG
 sed -i "/^addr-book-strict =/c\addr-book-strict = true" $CFG
 sed -i "/^pex =/c\pex = true" $CFG
+if [ "$METRICS_ENABLED" = "true" ]; then
+  sed -i "/^prometheus =/c\prometheus = true" $CFG
+  sed -i "/^prometheus-listen-addr =/c\prometheus-listen-addr = \"0.0.0.0:26660\"" $CFG
+fi
