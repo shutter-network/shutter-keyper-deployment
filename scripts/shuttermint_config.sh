295c678a6b7ec227187dc0b8b71a010102271bc3
Ulrich Petri <ulo@ulo.pe>
2024-06-20 23:58:49 +0200

    Fix shuttermint config script

--- scripts/shuttermint_config.sh
+++ scripts/shuttermint_config.sh
@@ -4,13 +4,13 @@ set -ex
 
 CFG=/data/chain/config/config.toml
 
-sed -i "/^bootstrap-peers =/c\bootstrap-peers = \"${SEED_NODES}\"" $CFG
+sed -i "/^bootstrap_peers =/c\bootstrap_peers = \"${SEED_NODES}\"" $CFG
 sed -i "/^moniker =/c\moniker = \"${SHUTTERMINT_MONIKER}\"" $CFG
-sed -i "/^genesis-file =/c\genesis-file = \"/info/genesis.json\"" $CFG
-sed -i "/^external-address =/c\external-address = \"${PUBLIC_IP}:26656\"" $CFG
-sed -i "/^addr-book-strict =/c\addr-book-strict = true" $CFG
+sed -i "/^genesis_file =/c\genesis_file = \"/info/genesis.json\"" $CFG
+sed -i "/^external_address =/c\external_address = \"${PUBLIC_IP}:26656\"" $CFG
+sed -i "/^addr_book_strict =/c\addr_book_strict = true" $CFG
 sed -i "/^pex =/c\pex = true" $CFG
 if [ "$METRICS_ENABLED" = "true" ]; then
   sed -i "/^prometheus =/c\prometheus = true" $CFG
-  sed -i "/^prometheus-listen-addr =/c\prometheus-listen-addr = \"0.0.0.0:26660\"" $CFG
+  sed -i "/^prometheus_listen_addr =/c\prometheus_listen_addr = \"0.0.0.0:26660\"" $CFG
 fi
