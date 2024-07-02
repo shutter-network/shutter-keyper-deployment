711e7e140be8b58b9df398671940c742ed37f9a1
Ulrich Petri <ulo@ulo.pe>
2024-07-02 15:00:00 +0200

    Make dev/chain script more reliable

--- dev/chain.sh
+++ dev/chain.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-docker run --rm -it --network snapshot-keyper_default curl-multi http://chain:26657/$1
-returnValue=$?
-if [ $returnValue -eq 125 ]; # Image not yet downloaded
-then
-    docker pull ghcr.io/curl/curl-container/curl-multi:master
-    docker tag ghcr.io/curl/curl-container/curl-multi:master curl-multi
-    docker run --rm -it --network snapshot-keyper_default curl-multi http://chain:26657/$1
-fi
+
+set -e
+
+NETWORK=$(docker compose config chain --format "json" | docker run --rm -i ghcr.io/jqlang/jq:1.7.1 '.networks.default.name' )
+
+docker run --rm -it --network "$NETWORK" ghcr.io/curl/curl-container/curl-multi:master http://chain:26657/$1
