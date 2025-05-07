839ed0ed0358aa2f44da6a7c782ab7d8bbf8b180
Ulrich Petri <ulo@ulo.pe>
2025-05-07 18:06:32 +0200

    Fix dev/chain.sh jq invocation
    The returned string needs to be raw (i.e. no quotes).

--- dev/chain.sh
+++ dev/chain.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-NETWORK=$(docker compose config chain --format "json" | docker run --rm -i ghcr.io/jqlang/jq:1.7.1 '.networks.default.name' )
+NETWORK=$(docker compose config chain --format "json" | docker run --rm -i ghcr.io/jqlang/jq:1.7.1 -r '.networks.default.name' )
 
 docker run --rm -it --network "$NETWORK" ghcr.io/curl/curl-container/curl-multi:master http://chain:26657/$1
