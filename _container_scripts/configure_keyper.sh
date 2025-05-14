b6c41d3d96e80faad98b85523917ef0a06bc0576
blockchainluffy <jainpunit1997@gmail.com>
2025-05-14 12:10:29 +0530

    updated docker compose and keyper configure script to enable keyper apis

--- _container_scripts/configure_keyper.sh
+++ _container_scripts/configure_keyper.sh
@@ -33,7 +33,8 @@ fi
 
 # Values set from assets container and compose env varibles
 sed -i "/^InstanceID/c\InstanceID = ${_ASSETS_INSTANCE_ID}" $CFG
-sed -i "/^HTTPEnabled/c\HTTPEnabled = ${_ASSETS_HTTP_ENABLED}" $CFG
+# sed -i "/^HTTPEnabled/c\HTTPEnabled = ${_ASSETS_HTTP_ENABLED}" $CFG
+sed -i "/^HTTPEnabled/c\HTTPEnabled = ${SHUTTER_HTTPENABLED}" $CFG
 sed -i "/^DatabaseURL/c\DatabaseURL = \"${SHUTTER_DATABASEURL}\"" $CFG
 sed -i "/^MaxNumKeysPerMessage/c\MaxNumKeysPerMessage = ${_ASSETS_MAX_NUM_KEYS_PER_MESSAGE}" $CFG
 sed -i "/^SyncStartBlockNumber/c\SyncStartBlockNumber = ${_ASSETS_SYNC_START_BLOCK_NUMBER}" $CFG
